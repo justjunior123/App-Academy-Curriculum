7dc68d0299d17a92c062f0ba218e82ee0d809eb2
justjunior89 <bballernumber5@sbcglobal.net>
2018-08-28 23:54:08 -0700

    RSPEC Practice

---
+++ W2D3/skeleton-2/spec/dessert_spec.rb
@@ -0,0 +1,62 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+
+  let(:chef) { double("chef", name: "Tucker") }
+  let(:brownie) { Dessert.new("brownie", 100, chef) }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(brownie.type).to eq("brownie")
+    end
+    it "sets a quantity" do
+      expect(brownie.quantity).to eq(100)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect{ brownie.ingredients("string") }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.ingredients).to eq([])
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      expect(brownie.ingredients).to ep(ingredients.shuffle)
+    end
+
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+    end
+
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+    end
+
+  end
+end
