661d9e6ecdb80046806ac4f0ed6369dccee2b360
justjunior89 <bballernumber5@sbcglobal.net>
2018-09-10 15:38:07 -0700

    creat sql database users/questions

--- W3D2/AA_Questions/import_db.sql
+++ W3D2/AA_Questions/import_db.sql
@@ -1,9 +1,25 @@
 CREATE TABLE users (
+
   id INTEGER PRIMARY KEY,
   fname VARCHAR(255) TEXT,
-  
+  lname VARCHAR(255) TEXT
+
 );
+
+  INSERT INTO
+  users (fname,lname)
+  VALUES
+  ("Cloud","Strife"),("Luke","Smith")
+
+----Questions
 
 CREATE TABLE questions (
 
+  id INTEGER PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  body TEXT NOT NULL,
+
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
 );
