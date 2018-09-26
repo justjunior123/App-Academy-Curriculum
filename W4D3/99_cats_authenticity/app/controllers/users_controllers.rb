b1cf89d58fbc05cf8d60b52c767fc5de50dda8ec
justjunior89 <bballernumber5@sbcglobal.net>
2018-09-26 14:27:43 -0700

    add more security to app

---
+++ W4D3/99_cats_authenticity/app/controllers/users_controllers.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new()
+    render :new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:passord,:username)
+  end
+end
