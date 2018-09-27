d36c7259df78b71f9485a74e05640752a2c7dea9
justjunior89 <bballernumber5@sbcglobal.net>
2018-09-27 12:17:43 -0700

    Create session token for cats app

--- W4D3/99_cats_authenticity/app/controllers/users_controllers.rb
+++ W4D3/99_cats_authenticity/app/controllers/users_controllers.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+      # 422 is the status code for an unprocessable entity.
+      # You can either pass the status code or status symbol.
+      # In other words, you can also return:
+      # render json: user.errors.full_messages, status: 422
+    end
 
   end
 
--- W4D3/99_cats_authenticity/config/routes.rb
+++ W4D3/99_cats_authenticity/config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
       post :deny
     end
   end
-
+  resource :session, only: [:new, :create, :destroy]
   root to: redirect('/cats')
 end
