fa58006c6586f3e4abf248a0f7586ea083567808
justjunior89 <bballernumber5@sbcglobal.net>
2018-09-27 19:01:06 -0700

    update UserController

--- W4D4/music_app/app/controllers/users_controller.rb
+++ W4D4/music_app/app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render json: :new
 
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      render json: :create
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render json: :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
