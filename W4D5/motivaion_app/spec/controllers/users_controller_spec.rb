d2b79a84324355eb4b920b2213c4b58ee8ca833f
justjunior89 <bballernumber5@sbcglobal.net>
2018-09-29 14:13:36 -0700

    Add resources to users and create users controller template

---
+++ W4D5/motivaion_app/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe UsersController, type: :controller do
+  # What will I be testing in the controller that will go to our routes.
+  
+   # - create; allow for a user to create a new account
+   # - Describe Get#show
+   # _ Describe Get#index
+
+
+end
