546be02c15e6604006d590a6b0c3a98602db8d1e
justjunior89 <bballernumber5@sbcglobal.net>
2018-10-02 10:41:52 -0700

    Make a template hompage template using a stylesheet and html

--- W4D5/motivaion_app/spec/controllers/users_controller_spec.rb
+++ W4D5/motivaion_app/spec/controllers/users_controller_spec.rb
@@ -1,9 +1,14 @@
 RSpec.describe UsersController, type: :controller do
   # What will I be testing in the controller that will go to our routes.
-  
+
    # - create; allow for a user to create a new account
    # - Describe Get#show
-   # _ Describe Get#index
+   # - Describe Get#index
+   # - Describe Post#create
+   # -
 
+  describe " Testing all the methods in the users_controller" do
+    
+  end
 
 end
