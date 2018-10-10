12a8ab5a7bc3bc8a331f88a2d729964240a19460
justjunior89 <bballernumber5@sbcglobal.net>
2018-10-10 14:35:15 -0700

    Create my bind

--- W6D1/arguements.js
+++ W6D1/arguements.js
@@ -31,3 +31,38 @@ function sum2(...someArgs) {
 }
 
 sum2(1,2,3,4,5);
+
+// bind with args
+
+// Rewrite your myBind method so that it can take both bind-time arguments
+// and call-time arguments.
+
+Function.prototype.myBind1 = function (ctx) {
+  const fn = this;
+  const bindArgs = Array.from(arguments).slice(1);
+  return function _boundFn() {
+    const callArgs = Array.from(arguments);
+    return fn.apply(ctx, bindArgs.concat(callArgs));
+  };
+};
+
+
+function curriedSum(numArgs) {
+  const numbers = [];
+
+  function _curriedSum(num) {
+    numbers.push(num);
+
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach((n) => { total += n; });
+
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
