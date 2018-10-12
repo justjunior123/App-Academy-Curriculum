2f0e33e6a80dd3a74d749ea09b9202adf0ed5ca8
justjunior89 <bballernumber5@sbcglobal.net>
2018-10-12 16:04:11 -0700

    Made tic tac toe with jquery

--- W6D2/jquery_tic_tac_toe/js/main.js
+++ W6D2/jquery_tic_tac_toe/js/main.js
@@ -1,6 +1,11 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");// require appropriate file
+const Game = require("./Node_js/game.js");// require appropriate file
+
+
 
 $( () => {
-  // Your code here
+  const rootView = $('.ttt');// Your code here
+  const game = new Game();
+
+  new View(game,rootView);
 });
--- W6D2/jquery_tic_tac_toe/js/ttt-view.js
+++ W6D2/jquery_tic_tac_toe/js/ttt-view.js
@@ -1,11 +1,65 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
 
-  bindEvents() {}
+    this.setupBoard();
+    this.bindEvents();
+  }
 
-  makeMove($square) {}
+  bindEvents() {
+    this.$el.on("click","li", (event => {
+      const $square = $(event.currentTarget);
+      this.makeMove($square);
+    }));
+  }
 
-  setupBoard() {}
+  makeMove($square) {
+    const pos = $square.data("pos");
+    const currentPlayer = this.game.currentPlayer;
+
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      alert("This " + e.msg.toLowerCase());
+      return;
+    }
+
+    $square.addClass(currentPlayer);
+
+    if (this.game.isOver()) {
+      // cleanup click handlers.
+      this.$el.off("click");
+      this.$el.addClass("game-over");
+
+      const winner = this.game.winner();
+      const $figcaption = $("<figcaption>");
+
+      if (winner) {
+        this.$el.addClass(`winner-${winner}`);
+        $figcaption.html(`You win, ${winner}!`);
+      } else {
+        $figcaption.html("It's a draw!");
+      }
+
+      this.$el.append($figcaption);
+    }
+  }
+
+  setupBoard() {
+    let $ul = $("<ul>");
+
+    for (var rows = 0; rows < 3; rows++) {
+      for (var columns = 0; columns < 3; columns++) {
+        let $li = $("<li>");
+        $li.data("pos",[rows,columns]);
+
+        $ul.append($li);
+      }
+    }
+
+    this.$el.append($ul);
+  }
 }
 
 module.exports = View;
